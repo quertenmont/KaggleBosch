cfca54e4f948cc243877ae26a36118c2dd3548cd
Loic <Loic.Quertenmont@gmail.com>
2016-09-01 16:01:53 +0200

    full with pretraining

--- tensorflow/DeepMLP.py
+++ tensorflow/DeepMLP.py
@@ -1,4 +1,5 @@
 import sys
+import math
 import time
 import re
 import collections
@@ -29,7 +30,7 @@ def readHeader(f):
     return groupHeader
 
 def castFloat(s):
-    if(s==''):return float(0)
+    if(s==''):return float(-1)
     return float(s)
 
 def readBatch(f, BatchSize, groupHeader, train=False, test=False, validation=False):
@@ -38,7 +39,9 @@ def readBatch(f, BatchSize, groupHeader, train=False, test=False, validation=Fal
    for g in groupHeader:
        data[g] = []
 
-   EOF = False
+   background = 0
+
+   EOF = False   
    while(len(data["Id"])<BatchSize and not EOF):
       try:
           #read a new line in the file
@@ -56,13 +59,16 @@ def readBatch(f, BatchSize, groupHeader, train=False, test=False, validation=Fal
           if(train      and (    testFlag or     valFlag) ): continue
           if(test       and (not testFlag or     valFlag) ): continue
           if(validation and (    testFlag or not valFlag) ): continue
+
+          if(train and background>int(BatchSize*0.3) and float(row[groupHeader["Response"][0]])==0):continue
+          if(float(row[groupHeader["Response"][0]])==0):background+=1
        
           #change datatype
           for g in groupHeader:
               i,j   = groupHeader[g]
               value = row[i:j]
               if   g == "Id":       value = [int(l) for l in value]
-              elif g == "Pattern":  value = [[int(l) for l in list(value[0])]]
+              elif g == "Pattern":  value = [int(l) for l in list(value[0])]
               elif g == "Response": value = [castFloat(l) for l in value]
               else : value = [ castFloat(l) for l in value  ]
 
@@ -75,31 +81,37 @@ def readBatch(f, BatchSize, groupHeader, train=False, test=False, validation=Fal
 
 
 def denseLayer(name, input, nIn, nOut):
-#    with tf.name_scope(name + "_"+str(nIn)+"In_"+str(nOut)+"Out") as scope:
-       W = tf.Variable(tf.truncated_normal([nIn,nOut]), name = "weight")
-       b = tf.Variable(tf.zeros([nOut]), name = "bias")
+    with tf.name_scope(name + "_"+str(nIn)+"to"+str(nOut)) as scope:
+       W = tf.Variable(tf.truncated_normal([nIn,nOut], stddev=math.sqrt(3 / (nIn+nOut))), name = "weight")
+       b = tf.Variable(tf.random_uniform([nOut], -0.1,0.1), name = "bias")
        return tf.sigmoid(tf.matmul(input, W) + b)
 
 def deepMLP(input, layers):
     results = input
     for i in range(1,len(layers)):
-        results = denseLayer("MLPlayer"+str(i), results, layers[i-1], layers[i])
-    return tf.sigmoid(results) #we want a probability in [0 , 1]
+        results = denseLayer("layer"+str(i), results, layers[i-1], layers[i])
+    return results #we want a probability in [0 , 1]
 
 #global variable
 nameToTFVariable = {}
-def stationNN(station, groupHeader):
+def stationNN(station, groupHeader, Pattern, index):
     global nameToTFVariable
     
     nOut  = 1
     nIn   = groupHeader[station][1] - groupHeader[station][0]
     input = tf.placeholder(tf.float32, shape=[None,nIn], name = station+"-input")    
     nameToTFVariable[station] = input
-#    with tf.name_scope(station) as scope:
-    #return deepMLP(input, [int(nIn), int(nIn*4/3), int(nIn*2/3), int(nOut)])
-    return deepMLP(input, [int(nIn), int(nOut)])
 
-        
+    with tf.name_scope(station) as scope:
+       out = deepMLP(input, [int(nIn), int(math.ceil(nIn*4/3)), int(math.ceil(nIn*2/3)),  int(nOut)])  #=> OUT OF MEMORY
+#       out = deepMLP(input, [int(nIn), int(nIn*4/3), int(nOut)])
+
+       with tf.name_scope("Gate") as scope:
+          gate = tf.expand_dims(Pattern[:,index],1)
+          gated = (gate*(out+1)) - 1          
+       return gated
+
+
 
 def getFeedDict(data):
     feedDict = {}
@@ -109,50 +121,100 @@ def getFeedDict(data):
     return feedDict
 
 
+def loss_log(out, target):
+    return - tf.reduce_sum( target * tf.log(out)  + (1.0-target)*tf.log(1.0-out) )
+
+def loss_hinge(out, target):
+    return tf.reduce_sum( tf.maximum( 0.0, 1.0 - ((2.0*target)-1.0) * ((2.0*out)-1.0)  ) )    
+
 
 ###############MAIN#####################
 
-batchSize = 1000
+batchSize = 250
+
+GroupList = ['L0_S00','L0_S01','L0_S02','L0_S03','L0_S04','L0_S05','L0_S06','L0_S07','L0_S08','L0_S09','L0_S10','L0_S11','L0_S12','L0_S13','L0_S14','L0_S15','L0_S16','L0_S17','L0_S18','L0_S19','L0_S20','L0_S21','L0_S22','L0_S23','L1_S24','L1_S25','L2_S26','L2_S27','L2_S28','L3_S29','L3_S30','L3_S31','L3_S32','L3_S33','L3_S34','L3_S35','L3_S36','L3_S37','L3_S38','L3_S39','L3_S40','L3_S41','L3_S42','L3_S43','L3_S44','L3_S45','L3_S46','L3_S47','L3_S48','L3_S49','L3_S50','L3_S51']
 
 #read the header and the validation sample
 with open('../normalized.csv', 'r') as f:
    groupHeader = readHeader(f)
    validation = readBatch(f, batchSize, groupHeader, validation=True)
+   #for g in groupHeader:   print(g)
 
 #build the graph of the neural network
 graph = tf.Graph()
 with graph.as_default():
+    #prepare the optimizer that we can use altrough
+    with tf.name_scope("trainer") as scope:
+        optimizer = tf.train.RMSPropOptimizer(0.1)#, use_locking=True)
+
+
+    #load variables
     Response = tf.placeholder(tf.float32, shape=[None,1], name = 'Response-input')
     nameToTFVariable["Response"] = Response
-    
-    with tf.name_scope("ML") as scope:
-        out_L0_S05 = stationNN("L0_S05", groupHeader)
 
-#        Hypothesis2 = stationNN("L0_S06", groupHeader)
+    with tf.name_scope("PerStationMLP") as scope:
+        Pattern = tf.placeholder(tf.float32, shape=[None,52], name = 'Pattern-gate')    
+        nameToTFVariable["Pattern"] = Pattern
 
-        for v in tf.trainable_variables():
-           print("trainame = " +v.name)
+        outAtStation =[]
+        pretrainingAtStation = []
+        for i,g in enumerate(GroupList):
+           out =  stationNN(g, groupHeader, Pattern, i)
+           outAtStation += [out]
 
+           with tf.name_scope("pre-training") as scope:
+              with tf.name_scope("loss") as scope:      
+                 patternMask = tf.greater(Pattern[:,i],0.5)
+                 stationloss = loss_hinge(tf.boolean_mask(out, patternMask), tf.boolean_mask(Response, patternMask))
+                 stationcost = stationloss
+                 
+              with tf.name_scope("trainer") as scope:
+                  training_var = []
+                  for v in tf.trainable_variables() :
+                      if(g in v.name):training_var += [v]
+                  grads = optimizer.compute_gradients(stationcost, var_list=training_var )#, var_list=training_var)
+
+                  pretrainingAtStation += [ optimizer.minimize(stationcost) ]
+#        pretraining = tf.group(pretrainingAtStation, name="pre-training")                 
+
+
+           
+
+    #recombine the output of each station into one single MLP
+    with tf.name_scope("RecombinationMLP") as scope:
+        outAtStationPack = tf.concat(1,outAtStation)
+        nStations = int(len(GroupList))
+        outFinal = deepMLP(outAtStationPack, [nStations, int(math.ceil(nStations*4/3)), int(math.ceil(nStations*2/3)), int(1)])
+
+#        for v in tf.trainable_variables(): print("trainame = " +v.name + " --> " + str("L0_S21" in v.name))
+#        for v in filter(lambda x:"L0_S21" in x.name, tf.trainable_variables()): print("trainame = " +v.name)
 
     with tf.name_scope("cost") as scope:
-        #subtle gymnastic to keep everything differentiable
-#        TP = 0.1 + tf.reduce_sum( tf.select(tf.logical_and(tf.   greater(Response, 0.5), tf.   greater(out_L0_S05, 0.5)) , tf.maximum(tf.minimum(out_L0_S05,1.0),1.0), tf.maximum(tf.minimum(out_L0_S05,0.0),0.0)  ) )
-#        TN = 0.1 + tf.reduce_sum( tf.select(tf.logical_and(tf.less_equal(Response, 0.5), tf.less_equal(out_L0_S05, 0.5)) , tf.maximum(tf.minimum(out_L0_S05,1.0),1.0), tf.maximum(tf.minimum(out_L0_S05,0.0),0.0)  ) )
-#        FN = 0.1 + tf.reduce_sum( tf.select(tf.logical_and(tf.   greater(Response, 0.5), tf.less_equal(out_L0_S05, 0.5)) , tf.maximum(tf.minimum(out_L0_S05,1.0),1.0), tf.maximum(tf.minimum(out_L0_S05,0.0),0.0)  ) )
-#        FP = 0.1 + tf.reduce_sum( tf.select(tf.logical_and(tf.less_equal(Response, 0.5), tf.   greater(out_L0_S05, 0.5)) , tf.maximum(tf.minimum(out_L0_S05,1.0),1.0), tf.maximum(tf.minimum(out_L0_S05,0.0),0.0)  ) )
-#        MCC  = 1 - tf.div ( tf.sub(tf.mul(TP,TN),tf.mul(FP,FN)) ,  tf.sqrt( tf.mul( tf.mul(tf.add(TP,FP),tf.add(TP,FN)) , tf.mul(tf.add(TN,FP),tf.add(TN,FN)) ) ) )
+        with tf.name_scope("L2") as scope:            
+           L2   = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()]) * 0.00001
+
+        with tf.name_scope("evaluation") as scope:
+           TP  = 0.0001 + tf.reduce_sum( tf.cast(tf.logical_and(tf.greater_equal(Response, 0.5) , tf.greater_equal(outFinal, 0.5) ), tf.float32) )
+           TN  = 0.0001 + tf.reduce_sum( tf.cast(tf.logical_and(tf.         less(Response, 0.5) , tf.         less(outFinal, 0.5) ), tf.float32) )
+           FP  = 0.0001 + tf.reduce_sum( tf.cast(tf.logical_and(tf.         less(Response, 0.5) , tf.greater_equal(outFinal, 0.5) ), tf.float32) )
+           FN  = 0.0001 + tf.reduce_sum( tf.cast(tf.logical_and(tf.greater_equal(Response, 0.5) , tf.         less(outFinal, 0.5) ), tf.float32) )
+           MCC = tf.div ( tf.sub(tf.mul(TP,TN),tf.mul(FP,FN)) , tf.sqrt( tf.mul( tf.mul(tf.add(TP,FP),tf.add(TP,FN)) , tf.mul(tf.add(TN,FP),tf.add(TN,FN)) ) ) )
+
+        with tf.name_scope("loss") as scope:      
+#           debug = tf.maximum( 0.0, 1.0 - (2.0*(Response-1.0)) * (2.0*(outFinal-1.0))  )
+           debug = 1.0 - ((2.0*Response)-1.0) * ((2.0*outFinal)-1.0)
+           crossentropy = loss_hinge(outFinal, Response)
+           cost = crossentropy# + L2
+        #cost = tf.Print(crossentropy, [Response, outFinal, TP, TN, FN, FP, MCC,  TPa, TNa, FPa, FNa, MCCa])  # + L2
 
 
-        crossentropy = - tf.reduce_sum( tf.add(tf.mul(Response,tf.log(out_L0_S05))  , tf.mul(tf.sub(1.0,Response), tf.log(tf.sub(1.0,out_L0_S05)) ) ) )
-        L2   = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()])*0.001
-        cost = crossentropy + L2 #  tf.Print(crossentropy + L2, [crossentropy, L2])  # + L2
+    with tf.name_scope("trainer") as scope:
+        training = optimizer.minimize(cost)#, gate_gradients=optimizer.GATE_OP)
 
-        TP = tf.reduce_sum( tf.cast(tf.logical_and(tf.greater_equal(Response, 0.5) , tf.greater_equal(out_L0_S05, 0.5) ), tf.float32) )
-        TN = tf.reduce_sum( tf.cast(tf.logical_and(tf.   less_equal(Response, 0.5) , tf.   less_equal(out_L0_S05, 0.5) ), tf.float32) )
-        FP = tf.reduce_sum( tf.cast(tf.logical_and(tf.greater_equal(Response, 0.5) , tf.   less_equal(out_L0_S05, 0.5) ), tf.float32) )
-        FN = tf.reduce_sum( tf.cast(tf.logical_and(tf.   less_equal(Response, 0.5) , tf.greater_equal(out_L0_S05, 0.5) ), tf.float32) )
-        MCC  = tf.div ( tf.sub(tf.mul(FP,FN),tf.mul(TP,TN)) ,  tf.sqrt( tf.mul( tf.mul(tf.add(TP,FP),tf.add(TP,FN)) , tf.mul(tf.add(TN,FP),tf.add(TN,FN)) ) ) )
+        #grads_and_vars = optimizer.compute_gradients(cost)
+        #gradsA = tf.Print(grads_and_vars[0][0], grads_and_vars)
 
+    with tf.name_scope("summary") as scope:        
         tf.scalar_summary('cost', cost)
         tf.scalar_summary('crossentropy', crossentropy)
         tf.scalar_summary('L2', L2)
@@ -161,18 +223,12 @@ with graph.as_default():
         tf.scalar_summary('FP', FP)
         tf.scalar_summary('FN', FN)
         tf.scalar_summary('MCC', MCC)
+        TFSummary = tf.merge_all_summaries()        
 
 
-    with tf.name_scope("trainer") as scope:
-        optimizer = tf.train.AdamOptimizer(0.01, use_locking=True)
-        training = optimizer.minimize(cost)#, gate_gradients=optimizer.GATE_OP)
-
-        grads_and_vars = optimizer.compute_gradients(cost)
-        gradsA = tf.Print(grads_and_vars[0][0], grads_and_vars)
-
-
-    TFSummary = tf.merge_all_summaries()        
     init = tf.initialize_all_variables()
+    saver        = tf.train.Saver()
+
 
 
 #create the running session on the graph
@@ -182,6 +238,7 @@ sess = tf.Session(graph=graph)
 writer_train = tf.train.SummaryWriter("./tflogs/train", graph)
 writer_val   = tf.train.SummaryWriter("./tflogs/validation", graph)
 sess.run(init)
+
 
 t_start = time.clock()
 totalLineRead = 0
@@ -196,6 +253,7 @@ with open('../normalized.csv', 'r') as f:
             f.seek(0)#rewind
             epoch         += 1
             totalLineRead += len(batch["Id"])
+            #saver.save(sess, "saves/pretraining.ckpt")
         else:
             totalLineRead += batchSize
 
@@ -203,12 +261,23 @@ with open('../normalized.csv', 'r') as f:
         #grads = sess.run([gradsA], feed_dict=getFeedDict(batch))
         #print(grads)
 
-        outTrain = sess.run([cost, TFSummary, training], feed_dict=getFeedDict(batch))
-        if iteration % 1 == 0:
+#        gate = sess.run([gatesA], feed_dict=getFeedDict(batch))
+#        print("Gates:"+str(gate))
+#        print("Patterns:"+str(batch["Pattern"]))
+
+        outTrain = sess.run([cost, TFSummary, outFinal, debug] + pretrainingAtStation, feed_dict=getFeedDict(batch))
+        if iteration % 10 == 0:
            outValid = sess.run([cost, TFSummary], feed_dict=getFeedDict(validation))
            print("Iteration=%5i Epoch=%2i LineRead=%8i --> cost (train) = %6.3f / cost (validation) = %+6.3f" % (iteration,epoch,totalLineRead,outTrain[0], outValid[0]))
+
+           compString = ""
+           for i,pred in enumerate(outTrain[2]):
+               if(i>10):break;
+               compString += "%s vs %s (%s),  " % ( str(batch["Response"][i][0]), float(pred), (outTrain[3][i][0]) )
+           print(compString)
            writer_train.add_summary(outTrain[1], totalLineRead)
            writer_val  .add_summary(outValid[1], totalLineRead)
+
 
 
         if(epoch>=10):break #stopping after nloop on the files
